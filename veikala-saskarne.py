4a168c4e5d732cd925ee1dce95d8c8d24aa5cfef
katriinakk <123467252+katriinakk@users.noreply.github.com>
2024-10-09 20:12:13 +0300

    asd
    asd

--- veikala-saskarne.py
+++ veikala-saskarne.py
@@ -10,20 +10,23 @@ class App():
         #Toplevel.__init__(self, master)
         self.master = master
         self.master.title("Iepirkumu grozs")
-        self.master.geometry("400x550")
+        self.master.geometry("800x550")
         self.cart_list = []
         self.cart = ShoppingCart("Makarūni", 9.10, 4)
         self.prod = Produkti("Makarūni", 9.10, 4)
-        self.product = 0
 
 
     def input_info_in(self):
         self.entry = Entry()
-        self.entry.grid(row = 1, column = 2, columnspan=4, padx =10, pady=10 )
+        self.entry.grid(row = 1, column = 2, columnspan=6, padx =10, pady=10 )
         self.entry2 = Entry()
-        self.entry2.grid(row = 2, column = 2, columnspan=4, padx =10, pady=10 )
+        self.entry2.grid(row = 2, column = 2, columnspan=6, padx =10, pady=10 )
         self.entry3 = Entry()
-        self.entry3.grid(row = 3, column = 2, columnspan=4, padx =10, pady=10 )
+        self.entry3.grid(row = 3, column = 2, columnspan=6, padx =10, pady=10 )
+        self.labelremove = Label(text = "Ievadiet produkta numuru,\nko vēlaties noņemt:")
+        self.labelremove.grid(row = 4, column =1, padx =10, pady=10)
+        self.removestuff = Entry()
+        self.removestuff.grid(row = 4, column = 2, columnspan=6, padx =10, pady=10)
 
     def input_label(self):
         self.label = Label(text= "Produkts:")
@@ -36,37 +39,74 @@ class App():
         self.label3.grid(row=3, column=1, padx =10, pady=10)
         #self.label.configure(command=self.cart.add_product_to_cart(self.entry3.get()))
 
-    def entry_input(self):
-        self.product = ShoppingCart(self.entry.get(), self.entry2.get(), self.entry3.get())
-        return self.product
 
-    def the_maths(self, aa):
-        self.cart.add_product_to_cart(aa)
-        lengthof = len(self.cart_list)
-        result = self.cart.get_total_price1(aa)
-        teksts = "Produktu tipu skaits: "+str(lengthof)+"; Kopējā (groza) summa: "+str(result)
 
-        self.labelout = Label()
+
+#    def remove_stuff(self):
+#        self.buttonremove = Button(text="Noņemt")
+#        self.buttonremove.grid(row = 6, column = 2, columnspan=4, padx =10, pady=10)
+#        self.buttonremove.configure(command=lambda: App.remove_process(self))
+
+
+
+    def the_maths(self):
+        if self.entry != None or self.entry2 != None or self.entry3 != None:
+            self.product = Produkti(self.entry.get(), float(self.entry2.get()), int(self.entry3.get()))
+            self.cart_list.append(self.product)
+
+            lengthof = len(self.cart_list)
+            total = 0
+            i = 0
+        
+            while i < lengthof:
+                total = total + self.cart_list[i].price*self.cart_list[i].quantity
+                i = i+1
+
+            teksts = "Produktu skaits: "+str(lengthof)+"; Kopējā (groza) summa: "+str(total)
+
+            self.labelout = Label(text = teksts)
+            self.labelout.grid(row=6, column=2, padx =10, pady=10)
+        else: 
+            pass
+        #return teksts
+
+    def products_in_my_cart(self):
+        i = 0
+        lengthh = len(self.cart_list)
+        texts = " "
+        while i < lengthh:         
+            texts = str(i+1)+". produkts: "+self.product.name
+            i=i+1
+
+        self.labelis = Label(text = texts)
+        self.labelis.grid(row=i, column= 3, padx =10, pady=10)
+
+    def remove_process(self):
+        num = int(self.removestuff.get())
+        if num != None:
+            x = self.cart_list.pop(num)
+            self.cart_list.remove(x)
+        else:
+            pass   
 
     def input_stuff(self):
         self.button = Button(text="Ievadīt")
-        self.button.grid(row=4, column= 2, padx =10, pady=10 )
-        self.button.configure(command=)
+        self.button.grid(row=5, column= 2, padx =10, pady=10 )
+        self.button.configure(command=lambda: [#App.remove_process(self),
+             App.the_maths(self), App.products_in_my_cart(self) ])
+
+
 
     
 
 
     
-
-
-
-
 logs = Tk()
 a = App(master=logs)
 a.input_info_in()
 a.input_label()
-outputs = a.entry_input
-a.the_maths(outputs)
+#outputs = a.entry_input()
+#a.the_maths(outputs)
 a.input_stuff()
 
 

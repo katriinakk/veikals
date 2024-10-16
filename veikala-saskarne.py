92f3fa13501d0377001fd7afb71789d1883a685a
katriinakk <123467252+katriinakk@users.noreply.github.com>
2024-10-16 21:41:49 +0300

    fff
    f

--- veikala-saskarne.py
+++ veikala-saskarne.py
@@ -11,7 +11,8 @@ class App():
         #Toplevel.__init__(self, master)
         self.master = master
         self.master.title("Iepirkumu grozs")
-        self.master.geometry("800x550")
+        self.master.geometry("800x700")
+        self.master.configure(bg='lightblue')
         self.cart_list = []
         self.cart = ShoppingCart("Makarūni", 9.10, 4)
         self.prod = Produkti("Makarūni", 9.10, 4)
@@ -19,13 +20,13 @@ class App():
 
     def input_info_in(self):
         self.entry = Entry()
-        self.entry.grid(row = 1, column = 2, columnspan=6, padx =10, pady=10 )
+        self.entry.grid(row = 1, column = 2, columnspan=1, padx =10, pady=10 )
         self.entry2 = Entry()
-        self.entry2.grid(row = 2, column = 2, columnspan=6, padx =10, pady=10 )
+        self.entry2.grid(row = 2, column = 2, columnspan=1, padx =10, pady=10 )
         self.entry3 = Entry()
-        self.entry3.grid(row = 3, column = 2, columnspan=6, padx =10, pady=10 )
+        self.entry3.grid(row = 3, column = 2, columnspan=1, padx =10, pady=10 )
         self.removestuff = Entry()
-        self.removestuff.grid(row = 4, column = 2, columnspan=6, padx =10, pady=10)
+        self.removestuff.grid(row = 4, column = 2, columnspan=1, padx =10, pady=10)
 
 
     def input_label(self):
@@ -40,6 +41,8 @@ class App():
         #self.label.configure(command=self.cart.add_product_to_cart(self.entry3.get()))
         self.labelremove = Label(text = "Ievadiet produkta numuru,\nko vēlaties noņemt:")
         self.labelremove.grid(row = 4, column =1, padx =10, pady=10)
+        self.teksts = Text(self.master, height = 20, width = 20)
+        self.teksts.grid(row=1, column = 4, padx = 10, pady = 10, rowspan = 4)
 
 
 
@@ -81,6 +84,7 @@ class App():
 
     
         self.labelout = Label(text = "       "+teksts+"       ", bg = 'light gray')
+        self.labelout.config(text = "       "+teksts+"       ")
         self.labelout.grid(row=6, column=2, padx =10, pady=10)
 
         #return teksts
@@ -88,13 +92,13 @@ class App():
     def products_in_my_cart(self):
         i = 0
         lengthh = len(self.cart_list)
-        texts = " "
+        self.texts = ""
         while i < lengthh:         
-            texts = str(i+1)+". produkts: "+self.cart_list[i].name
+            self.texts = self.texts+"\n"+str(i+1)+". produkts: "+self.cart_list[i].name+"\n"
             i=i+1
+        self.teksts.insert(tk.END, "----------\n"+self.texts)
 
-        self.labelis = Label(text = texts, bg = 'light gray')
-        self.labelis.grid(row=i, column= 3, padx =10, pady=10)
+
 
     def remove_process(self):
 
@@ -107,11 +111,24 @@ class App():
             print(self.num)
         except:
             pass   
+        
+    def destroy_text(self):
+        self.teksts.delete("1.0","end") 
+        self.teksts = Text(self.master, height = 20, width = 20)
+        self.teksts.grid(row=1, column = 4, padx = 10, pady = 10, rowspan = 4)  
 
     def input_stuff(self):
         self.button = Button(text="Ievadīt")
         self.button.grid(row=5, column= 2, padx =10, pady=10 )
         self.button.configure(command=lambda: [App.remove_process(self), App.the_maths(self), App.products_in_my_cart(self) ])
+        self.button2 = Button(text = "notīrīt lauciņus")
+        self.button2.grid(row=5, column= 1, padx =10, pady=10 )
+        self.button2.configure(command=lambda: [self.entry.delete(0, tk.END), self.entry2.delete(0, tk.END), self.entry3.delete(0, tk.END)])
+        self.button3 = Button(text = "Notīrīt izvadi")
+        self.button3.grid(row=5, column= 4, padx =10, pady=10 )
+        self.button3.configure(command=lambda: App.destroy_text(self))
+
+
 
     def image(self,master):
         self.image = Image.open("french.jpg")
@@ -120,6 +137,8 @@ class App():
  
         self.labeli = tk.Label(master, image=self.image2)
         self.labeli.grid(row=7, column= 2, padx =10, pady=10 )
+        self.chat = Label(text = "oui oui")
+        self.chat.grid(row=7, column= 3, padx =10, pady=10)
 
     
 logs = Tk()
